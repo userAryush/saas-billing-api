cfac12b2cc16198371c0c510237a05e15b07a4f8
userAryush <khatriaryush@gmail.com>
2026-06-03 22:16:06 +0545

    created accounts app with custom user model and update settings for postgres

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db.models import EmailField, CharField, BooleanField, DateTimeField
+
+# PermissionMixin -> Without it, user has no is_superuser, no groups, no user_permissions - Django admin breaks and has_perm() won't work.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = EmailField(unique=True)
+    full_name = CharField(max_length=150)
+    is_active = BooleanField(default=True)
+    is_staff = BooleanField(default=False)
+    created_at = DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']    
+    
--- saas_billing_api/settings.py
+++ saas_billing_api/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'accounts.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,22 @@ WSGI_APPLICATION = 'saas_billing_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'billing_api',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
