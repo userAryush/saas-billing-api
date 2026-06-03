97ab60ed137050a5996a9370018c62737e8b33a0
userAryush <khatriaryush@gmail.com>
2026-06-03 22:20:22 +0545

    added custom user manager for user creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db.models import EmailField, CharField, BooleanField, DateTimeField
-
+from .usermanager import UserManager
 # PermissionMixin -> Without it, user has no is_superuser, no groups, no user_permissions - Django admin breaks and has_perm() won't work.
 class User(AbstractBaseUser, PermissionsMixin):
     email = EmailField(unique=True)
@@ -11,4 +11,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     created_at = DateTimeField(auto_now_add=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']    
-    
+    objects = UserManager()
